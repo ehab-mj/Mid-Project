ff1cc4153053cc87a4af36c62aef67ee49123729
ehab magdob <ehabmj1212@gmail.com>
2026-02-26 18:32:25 +0200

    "

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -43,7 +43,7 @@ export default function NavBar() {
                     </Link>
                 )}
 
-                {/* <Search /> */}
+                <Search />
 
                 <div>
                     {AuthUser ? (
