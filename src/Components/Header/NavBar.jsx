055e461eb5b7a0da4ec5cf47ad66898c69067c6d
ehab magdob <ehabmj1212@gmail.com>
2026-03-02 15:11:37 +0200

    To main

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -80,7 +80,6 @@ export default function NavBar() {
                 </div >
             </nav>
 
-
             {open && (
                 <LoginModal
                     onClose={() => setOpen(false)}
